b8807c48f66bc34cf0ba536d78e58f59aa760f46
Efren Rodriguez <efren.rodriguez.sa@gmail.com>
2023-02-25 12:31:39 -0600

    anadimos cambios

--- src/main/java/com/mx/CrudEmpresaA/CrudEmpresaAApplication.java
+++ src/main/java/com/mx/CrudEmpresaA/CrudEmpresaAApplication.java
@@ -1,12 +1,24 @@
 package com.mx.CrudEmpresaA;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CrudEmpresaAApplication {
-
+	
+	public static Logger logger = LoggerFactory.getLogger(CrudEmpresaAApplication.class);
+	
+	@PostConstruct
+	public void intt() {
+		logger.info("Aplication started...");
+	}
+	
 	public static void main(String[] args) {
+		logger.info("Aplication executed...");
 		SpringApplication.run(CrudEmpresaAApplication.class, args);
 	}
 
--- src/test/java/com/mx/CrudEmpresaA/CrudEmpresaAApplicationTests.java
+++ src/test/java/com/mx/CrudEmpresaA/CrudEmpresaAApplicationTests.java
@@ -1,13 +1,21 @@
 package com.mx.CrudEmpresaA;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CrudEmpresaAApplicationTests {
 
+	public static Logger logger = LoggerFactory.getLogger(CrudEmpresaAApplication.class);
+	
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		logger.info("Test case executing...");
+		assertEquals(true, true);
 	}
 
 }
